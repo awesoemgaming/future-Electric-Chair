5545fe39a83eaf6fc2dda2e7cb05e418d484583e
awesoemgaming <129987735+awesoemgaming@users.noreply.github.com>
2023-05-14 20:30:41 -0700

    Create Config.lua

---
+++ Electric/Config.lua
@@ -0,0 +1,28 @@
+Config = {}
+
+Config.killcmd = "execute" -- the command to electrecute the person on the chair
+
+Config.chairdist = 2.0 -- how close you must be to the chair.
+
+Config.setpos = vector3(428.81, -976.24, 30.75) -- where to set the player being electrecuted
+
+Config.setheading = 176.6 -- teh heading to set the player being electrecuted
+
+Config.sitaniamtiondict = "timetable@ron@ig_3_couch" -- the aniamtio dictinoary that plays when player sits on the chair.
+
+Config.sitanimtype = "base" -- the animation type
+
+Config.notnearchair = "You are not close enough to the chair" -- message that displays when player isn't close enough to the chair.
+
+Config.chairspawn = vector3(428.82, -975.81, 29.6) -- where the electric chair spawns (make sure this matches with what your heading and position is)
+
+Config.chairmodel = "prop_old_wood_chair" -- the prop name of the chair.
+
+Config.healthlosspertick = 10 -- how much health you lose every tick. Config the tick time below.
+
+Config.losehealthspeed = 5000 -- how often you lose health while being electrocuted. In milisecconds, so 5000 = 5 secconds.
+
+-- Blindfold config
+Config.masknum = 49 -- this is the number for the mask on your clothing menu.
+
+Config.masktex = 19 -- this is the number for the mask texture on your clothing menu.
